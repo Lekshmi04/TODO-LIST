780e7008e5419734432a43d2d1b11247b3b763b7
lekshmi <lekshmipr04@gmail.com>
2020-12-13 14:55:51 +0530

    todo

---
+++ todo-list/src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from './Todo';
+function TodoList({todos ,togglecomplete,removeTodo}){
+     return(
+        <ul>
+            {todos.map(todo=>
+                <Todo key={todo.id}
+                 todo={todo} 
+                 togglecomplete={togglecomplete}
+                 removeTodo={removeTodo} />)}
+                </ul>
+        
+     );
+}
+export default TodoList;
